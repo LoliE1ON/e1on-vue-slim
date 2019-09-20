f5ef74ff01d30d49d9361a02ebf8a566efba96d6
d0kur0 <dev.dokuro@gmail.com>
2019-09-20 22:12:22 +0300

    Remove

--- src/components/Footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-        <div class="uk-card uk-card-default app-footer">
-
-            Footer
-        
-        </div>
-    </div>
-</template>
--- src/config/discord.js
+++
@@ -1,5 +0,0 @@
-export default {
-    
-    API_WIDGET_URL: "https://discordapp.com/api/guilds/509065700401348630/widget.json",
-
-};
